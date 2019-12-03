71591379a497e82d49c66c9e30d6588ec1761742
Matthew Zhang <mtz99@users.noreply.github.com>
2019-12-02 21:33:22 -0800

    Part 1 of Day 1 finished

--- Day1/Day_1.py
+++ Day1/Day_1.py
@@ -1,12 +1,20 @@
 import sys
 import os
 
+
 def main():
-    path = '/Users/matzh/Desktop/AdventOfCode/Day1/input.txt'
+    calcd = []
+    #Scan file path and read through it
+    path = '/Users/matzh/Desktop/AdventOfCode2019/Day1/input.txt'
     with open(path) as File:
         x = File.readlines()
+        for i in x:
+            x = int(i)
+            calcd.append(int(x/3)-2)
+            print(calcd)
     File.close()
+    print(sum(calcd))
 
-    print(x)
+    #print(x)
 
 main()
